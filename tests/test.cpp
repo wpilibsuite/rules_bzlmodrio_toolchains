d809be0f1c98f0bce5377fb251efa6c3bf584b56
PJ Reiniger <pj.reiniger@gmail.com>
2026-06-10 22:33:49 -0400

    Upgrade to 2027-1 toolchain

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,15 +2,9 @@
 #include "gtest/gtest.h"
 
 TEST(BasicTest, Basic) {
-#ifdef __FRC_ROBORIO__
-  EXPECT_EQ(1, IS_ROBORIO_TEST);
+#ifdef __FIRST_SYSTEMCORE__
+  EXPECT_EQ(1, IS_SYSTEMCORE_TEST);
 #else
-  EXPECT_EQ(0, IS_ROBORIO_TEST);
-#endif
-
-#ifdef __RASPBIAN__
-  EXPECT_EQ(1, IS_RASPBIAN_TEST);
-#else
-  EXPECT_EQ(0, IS_RASPBIAN_TEST);
+  EXPECT_EQ(0, IS_SYSTEMCORE_TEST);
 #endif
 }
